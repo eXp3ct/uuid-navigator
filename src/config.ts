967a50154c73ff489a820fb7390804bd98a7c4f2
rveselov <r.veselov@bmc-soft.ru>
2025-07-24 17:10:27 +0300

    apply styles config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 
 export interface ExtensionConfig {
+  applyStyles: boolean;
   highlightColor: string;  // Цвет текста UUID
   underline: boolean;      // Подчёркивание
   backgroundColor: string; // Фон UUID (HEX с альфа-каналом)
@@ -10,6 +11,7 @@ export interface ExtensionConfig {
 export function getConfig(): ExtensionConfig {
   const config = vscode.workspace.getConfiguration('uuidNavigator');
   return {
+    applyStyles: config.get('applyStyles', true),
     highlightColor: config.get('highlightColor', '#569CD6'),
     underline: config.get('underline', true),
     backgroundColor: config.get('backgroundColor', '#64c8ff1a'), // HEX с альфа-каналом
